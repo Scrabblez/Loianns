c4bd4cb21f4c361a04d78d154114db885b9f7d24
Gary Taylor <gptaylor@yorkcountypa.gov>
2018-11-13 20:32:28 -0500

    Added a user

--- SoulSong/Models/User.cs
+++ SoulSong/Models/User.cs
@@ -8,7 +8,19 @@ namespace SoulSong.Models
 {
     class User
     {
+
+        public User()
+        {
+            Name = "Loiann";
+            Address = "Whatever";
+        }
+
         public string Name { get; set; }
         public string Address { get; set; }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Yay");
+        }
     }
 }
