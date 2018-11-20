66b3973e7b84f41b5ee8a0f8c789c7ab2d1b7daf
Gary Taylor <gptaylor@yorkcountypa.gov>
2018-11-20 11:07:21 -0500

    Created MVVM Patter
    Broke out the WPF project into the MVVM pattern basics. Added some of basics of the MVVM pattern framework.

---
+++ SoulSong_Analysis/SoulSong_Analysis/ViewModels/InstallScreenVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism;
+using Prism.Commands;
+
+namespace SoulSong_Analysis.ViewModels
+{
+    class InstallScreenVM : BaseViewModel
+    {
+        public InstallScreenVM()
+        {
+            InstallButtonCommand = new DelegateCommand(Install);
+            CancelButtonCommand = new DelegateCommand(Cancel);
+        }
+        public DelegateCommand InstallButtonCommand { get; private set; }
+        public DelegateCommand CancelButtonCommand { get; private set; }
+
+        public void Install()
+        {
+            MessageBox.Show("I did a thing.");
+        }
+
+        public void Cancel()
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
---
+++ SoulSong_Analysis/SoulSong_Analysis/Views/InstallScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoulSong_Analysis.Views
+{
+    /// <summary>
+    /// Interaction logic for InstallWindow.xaml
+    /// </summary>
+    public partial class InstallScreen
+    {
+        /// Open InstallScreen
+        /// public InstallWindow()
+        /// {
+        /// InitializeComponent();
+        /// }
+
+        /// Starts Install Process
+        private void btnInstall_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        /// Closes Application
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        
+    }
+}
