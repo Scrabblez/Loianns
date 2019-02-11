5cf56a3dddf09fca3122866379dacfdc0f42917d
Mam1606 <crystalsingingbowls@gmail.com>
2019-02-11 09:58:09 -0500

    Separate Install Screen (VB)

---
+++ InstallScreen/InstallScreen/InstallScreen.vb
@@ -0,0 +1,17 @@
+﻿Public Class InstallScreen
+    Private Sub InstallScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblContact.Text = "Contact: crystalsingingbowls@gmail.com"
+        lblCopyright.Text = "Copyright © 2017 Ann Reeves"
+        lblVersion.Text = "Version: 1.0"
+    End Sub
+
+    Private Sub btnInstall_Click(sender As Object, e As EventArgs) Handles btnInstall.Click
+        'Code to install program
+        'If installed go to Splash Screen
+
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+End Class
