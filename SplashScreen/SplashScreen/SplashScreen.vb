d99538e6575f92a600313a9c95731ff170e7aa9f
Mam1606 <crystalsingingbowls@gmail.com>
2019-02-11 10:06:23 -0500

    Separate Splash Screen

---
+++ SplashScreen/SplashScreen/SplashScreen.vb
@@ -0,0 +1,7 @@
+﻿Public Class SplashScreen
+    Private Sub SplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblContact.Text = "Contact: crystalsingingbowls@gmail.com"
+        lblCopyright.Text = "Copyright © 2017 Ann Reeves"
+        lblVersion.Text = "Version: 1.0"
+    End Sub
+End Class
